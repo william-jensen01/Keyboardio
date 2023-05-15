81d1751b91ede60d725174ba11489c5beef0dde7
William Jensen <williambjensen01@gmail.com>
2023-05-15 17:09:31 -0600

    experiments that led to nothing new

--- src/components/Posts/PostsDashboard.js
+++ src/components/Posts/PostsDashboard.js
@@ -12,132 +12,129 @@ import { ReactComponent as Wave } from "../../assets/wave.svg";
 import "../../css/PostsDashboard.css";
 
 function PostsDashboard() {
-  const params = useParams();
-  const history = useHistory();
-  const location = useLocation();
+	const params = useParams();
+	const history = useHistory();
+	const location = useLocation();
 
-  // initialize state
-  const [numPages, setNumPages] = useState(null); // pagination
-  const [isLoading, setIsLoading] = useState(false); // used to show post skeleton loading
-  const [currentPosts, setCurrentPosts] = useState([]);
-  const [referrer, setReferrer] = useState(null); // when true, will contain a url to redirect page. Used for updating url info (postType, dateType, page, limit)
-  const [cardStyle] = useState("small"); // allow the ability to change card style preference in the future
+	// initialize state
+	const [numPages, setNumPages] = useState(null); // pagination
+	const [isLoading, setIsLoading] = useState(false); // used to show post skeleton loading
+	const [currentPosts, setCurrentPosts] = useState([]);
+	const [referrer, setReferrer] = useState(null); // when true, will contain a url to redirect page. Used for updating url info (postType, dateType, page, limit)
+	const [cardStyle] = useState("small"); // allow the ability to change card style preference in the future
 
-  // (**) => ignore; (--) => what it is
-  // url path params -> **website url**/posts/--dateType--/--postType--?**query params**
-  const dateType = params["dateType"];
-  const postType = params["postType"];
-  // url query params -> **urlpath**?page=__&limit=__
-  let currentUrlParams = new URLSearchParams(location.search);
-  const page = Number(currentUrlParams.get("page"));
-  const limit = Number(currentUrlParams.get("limit"));
+	// (**) => ignore; (--) => what it is
+	// url path params -> **website url**/posts/--dateType--/--postType--?**query params**
+	const dateType = params["dateType"];
+	const postType = params["postType"];
+	// url query params -> **urlpath**?page=__&limit=__
+	let currentUrlParams = new URLSearchParams(location.search);
+	const page = Number(currentUrlParams.get("page"));
+	const limit = Number(currentUrlParams.get("limit"));
 
-  // used in FilterPosts to apply new filters
-  const changeReferrer = (newUrl) => {
-    setReferrer(newUrl);
-  };
-  // used by Pagination to update/change the page by redirect
-  const changePage = (newPage) => {
-    setReferrer(
-      `/posts/${dateType}/${postType}?page=${newPage}&limit=${limit}`
-    );
-  };
+	// used in FilterPosts to apply new filters
+	const changeReferrer = (newUrl) => {
+		setReferrer(newUrl);
+	};
+	// used by Pagination to update/change the page by redirect
+	const changePage = (newPage) => {
+		setReferrer(
+			`/posts/${dateType}/${postType}?page=${newPage}&limit=${limit}`
+		);
+	};
 
-  // set referrer to null when location changes to allow switching from back and forth. Whenever we want to change the page through Paginate referrer can't already exist so this is how we 'reset' it.
-  // What problem or bug does this solve? If you are on page 2 and you click on 3 you will be redirected to page 3. If you go back to the previous page through the browser or the back mouse button, you will go back to page 2 (because it's on the history stack). If you were to then click on 3 it wouldn't do anything because it's trying to redirect to the previous referrer state (page 2) which is the page you are currently on. Hence, not changing the page
-  useEffect(() => {
-    setReferrer(null);
-  }, [location]);
+	// set referrer to null when location changes to allow switching from back and forth. Whenever we want to change the page through Paginate referrer can't already exist so this is how we 'reset' it.
+	// What problem or bug does this solve? If you are on page 2 and you click on 3 you will be redirected to page 3. If you go back to the previous page through the browser or the back mouse button, you will go back to page 2 (because it's on the history stack). If you were to then click on 3 it wouldn't do anything because it's trying to redirect to the previous referrer state (page 2) which is the page you are currently on. Hence, not changing the page
+	useEffect(() => {
+		setReferrer(null);
+	}, [location]);
 
-  // scroll to top of page, fetch posts from database and set states with the data whenever history or location (information in the url) changes
-  useEffect(() => {
-    window.scroll(0, 0); // note: I'm not sure why this will sometimes scroll to the top. It works 100% when clicking on any num in pagination bar. But when clicking the browsers back arrow or hitting back button on mouse it will sometimes work
+	// scroll to top of page, fetch posts from database and set states with the data whenever history or location (information in the url) changes
+	useEffect(() => {
+		window.scroll(0, 0); // note: I'm not sure why this will sometimes scroll to the top. It works 100% when clicking on any num in pagination bar. But when clicking the browsers back arrow or hitting back button on mouse it will sometimes work
 
-    setIsLoading(true);
+		setIsLoading(true);
 
-    // take the postType and convert into abbreviations
-    // example: 'interest-checks' will return 'ic'
-    const abbreviatePostType = () => {
-      if (postType === "interest-checks") {
-        return "ic";
-      } else if (postType === "group-buys") {
-        return "gb";
-      } else if (postType === "all") {
-        return "all";
-      }
-    };
-    let formattedPostType = abbreviatePostType();
+		// take the postType and convert into abbreviations
+		// example: 'interest-checks' will return 'ic'
+		const abbreviatePostType = () => {
+			if (postType === "interest-checks") return "ic";
+			if (postType === "group-buys") return "gb";
+			if (postType === "all") return "all";
+		};
+		let formattedPostType = abbreviatePostType();
 
-    async function fetchData() {
-      try {
-        const res = await axios.get(
-          `${process.env.REACT_APP_API_URL}/${dateType}/${formattedPostType}?page=${page}&limit=${limit}`
-        );
-        setIsLoading(false);
-        setCurrentPosts(res.data.posts);
-        setNumPages(res.data.pages);
-      } catch (err) {
-        history.go(0);
-      }
-    }
+		async function fetchData() {
+			try {
+				const res = await axios.get(
+					`${process.env.REACT_APP_API_URL}/posts/${formattedPostType}/${dateType}?page=${page}&limit=${limit}`
+				);
+				setIsLoading(false);
+				setCurrentPosts(res.data.posts);
+				setNumPages(res.data.page_info.total_pages);
+			} catch (err) {
+				// history.go(0);
+				console.log(err);
+			}
+		}
 
-    fetchData();
-  }, [history, dateType, postType, page, limit]);
+		fetchData();
+	}, [history, dateType, postType, page, limit]);
 
-  return (
-    <div className="posts-dashboard-container">
-      {referrer && <Redirect push to={referrer} />}
-      <div className="posts-main-content">
-        <div className="posts-dashboard-header">
-          <h1 className="posts-dashboard-title">
-            {`${
-              (dateType === "latest" && "Latest") ||
-              (dateType === "newest" && "Newest")
-            } ${
-              (postType === "all" && "Posts") ||
-              (postType === "interest-checks" && "Interest Checks") ||
-              (postType === "group-buys" && "Group Buys")
-            }`}
-          </h1>
+	return (
+		<div className="posts-dashboard-container">
+			{referrer && <Redirect push to={referrer} />}
+			<div className="posts-main-content">
+				<div className="posts-dashboard-header">
+					<h1 className="posts-dashboard-title">
+						{`${
+							(dateType === "latest" && "Latest") ||
+							(dateType === "newest" && "Newest")
+						} ${
+							(postType === "all" && "Posts") ||
+							(postType === "interest-checks" && "Interest Checks") ||
+							(postType === "group-buys" && "Group Buys")
+						}`}
+					</h1>
 
-          <FilterPosts
-            dateType={dateType}
-            postType={postType}
-            limit={limit}
-            changeReferrer={changeReferrer}
-          />
-        </div>
+					<FilterPosts
+						dateType={dateType}
+						postType={postType}
+						limit={limit}
+						changeReferrer={changeReferrer}
+					/>
+				</div>
 
-        <div className="posts-container">
-          {isLoading &&
-            Array.from(Array(limit), (e, i) => (
-              <PostSkeleton cardStyle={cardStyle} key={i + 1} />
-            ))}
-          {isLoading === false &&
-            currentPosts.length > 0 &&
-            currentPosts.map((post) => (
-              <LazyLoad
-                key={post.id}
-                placeholder={<PostSkeleton cardStyle={cardStyle} />}
-                once
-              >
-                <Post post={post} cardStyle={cardStyle} />
-              </LazyLoad>
-            ))}
-        </div>
+				<div className="posts-container">
+					{isLoading &&
+						Array.from(Array(limit), (e, i) => (
+							<PostSkeleton cardStyle={cardStyle} key={i + 1} />
+						))}
+					{isLoading === false &&
+						currentPosts.length > 0 &&
+						currentPosts.map((post) => (
+							<LazyLoad
+								key={post.id}
+								placeholder={<PostSkeleton cardStyle={cardStyle} />}
+								once
+							>
+								<Post post={post} cardStyle={cardStyle} />
+							</LazyLoad>
+						))}
+				</div>
 
-        {currentPosts.length > 0 && (
-          <Paginate
-            totalPageCount={numPages}
-            currentPage={page}
-            changePage={changePage}
-          />
-        )}
-      </div>
+				{currentPosts.length > 0 && (
+					<Paginate
+						totalPageCount={numPages}
+						currentPage={page}
+						changePage={changePage}
+					/>
+				)}
+			</div>
 
-      <Wave className="posts-wave" />
-    </div>
-  );
+			<Wave className="posts-wave" />
+		</div>
+	);
 }
 
 export default PostsDashboard;
